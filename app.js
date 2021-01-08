f35b6818ea60317aa524e450d81419699c645843
Amir Pahadi <apahadi73@gmail.com>
2021-01-08 14:44:53 -0600

    working on game over functionality

---
+++ app.js
@@ -0,0 +1,60 @@
+function getRandomValue(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const app = Vue.createApp({
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+      //   manages current round so that user can use special attack after every three rounds
+      currentRound: 0,
+    };
+  },
+  computed: {
+    monsterBarStyles() {
+      return { width: this.monsterHealth + "%" };
+    },
+    playerBarStyles() {
+      return { width: this.playerHealth + "%" };
+    },
+    // disable special attack if the current round variable in the data
+    // is not divisible by 3
+    canUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
+    },
+  },
+  methods: {
+    attackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(5, 12);
+      this.monsterHealth -= attackValue;
+      //   calls attackPlayer automatically
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      const attackValue = getRandomValue(8, 15);
+      this.playerHealth -= attackValue;
+    },
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(10, 15);
+      this.monsterHealth -= attackValue;
+      //   calls attackPlayer automatically
+      this.attackPlayer();
+    },
+    healPlayer() {
+      this.currentRound++;
+      const healValue = getRandomValue(8, 20);
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth += healValue;
+      }
+      //   calls attackPlayer automatically
+      this.attackPlayer();
+    },
+  },
+});
+
+app.mount("#game");
